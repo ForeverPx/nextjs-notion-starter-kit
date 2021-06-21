98cd071e93cfad0bc46e7b7e18a7d0982433db64
Maxpan <1315592944@qq.com>
2021-06-21 13:46:54 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '1ef8537b61aa4c798bf11e6183aae4f0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Foreverpx',
+  domain: 'foreverpx.cn',
+  author: 'Foreverpx',
 
   // open graph metadata (optional)
   description: 'Example site description',
